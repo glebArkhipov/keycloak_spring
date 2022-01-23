2b36467c30c598de0c6a74036e18b61e94f7684e
Gleb Arkhipov <gleber1995@gmail.com>
2022-01-23 22:44:46 +0300

    use app as resource server

--- src/main/kotlin/com/keycloakspring/WebSecurityConfig.kt
+++ src/main/kotlin/com/keycloakspring/WebSecurityConfig.kt
@@ -1,28 +1,34 @@
 package com.keycloakspring
 
-import net.minidev.json.JSONArray
+import com.nimbusds.jose.shaded.json.JSONArray
+import com.nimbusds.jose.shaded.json.JSONObject
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.convert.converter.Converter
+import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest
-import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser
-import org.springframework.security.oauth2.core.oidc.user.OidcUser
-import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
+import java.util.stream.Collectors
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     override fun configure(http: HttpSecurity) {
         http
-            .oauth2Login()
-            .userInfoEndpoint()
-            .oidcUserService(oidcUserService())
+            .oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(jwtAuthenticationConverter())
             .and()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .authorizeRequests()
             .antMatchers("/anonymous/**").permitAll()
@@ -30,15 +36,38 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
     }
 
     @Bean
-    fun oidcUserService(): OAuth2UserService<OidcUserRequest, OidcUser> {
-        val oidcServiceDelegate = OidcUserService()
-        return OAuth2UserService { userRequest ->
-            val oidcUser = oidcServiceDelegate.loadUser(userRequest)
-            val authorities = oidcUser.authorities
-                .filterIsInstance<OidcUserAuthority>()
-                .map { it.authority }
-                .map { SimpleGrantedAuthority(it) }
-            DefaultOidcUser(authorities, oidcUser.idToken, oidcUser.userInfo)
+    fun jwtAuthenticationConverter(): Converter<Jwt, AbstractAuthenticationToken> {
+        val jwtAuthenticationConverter = JwtAuthenticationConverter()
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter())
+        return jwtAuthenticationConverter
+    }
+
+    @Bean
+    fun jwtGrantedAuthoritiesConverter(): Converter<Jwt, Collection<GrantedAuthority>> {
+        val delegate = JwtGrantedAuthoritiesConverter()
+        return JwtToAuthority(delegate)
+    }
+}
+
+class JwtToAuthority(
+    private val delegate: JwtGrantedAuthoritiesConverter
+): Converter<Jwt, Collection<GrantedAuthority>> {
+    override fun convert(jwt: Jwt): Collection<GrantedAuthority> {
+        val grantedAuthorities: MutableCollection<GrantedAuthority> = delegate.convert(jwt) as MutableCollection<GrantedAuthority>
+        if (jwt.getClaim<Any?>("realm_access") == null) {
+            return grantedAuthorities
         }
+        val realmAccess: JSONObject = jwt.getClaim("realm_access")
+        if (realmAccess["roles"] == null) {
+            return grantedAuthorities
+        }
+        val roles: JSONArray = realmAccess["roles"] as JSONArray
+        val keycloakAuthorities: List<GrantedAuthority> = roles.stream().map { role ->
+            SimpleGrantedAuthority(
+                "ROLE_$role"
+            )
+        }.collect(Collectors.toList())
+        grantedAuthorities.addAll(keycloakAuthorities)
+        return grantedAuthorities
     }
 }
