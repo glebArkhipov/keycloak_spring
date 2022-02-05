542e766e39eddad8513c809a34d0f6c1028a9b61
Gleb Arkhipov <gleber1995@gmail.com>
2022-02-05 12:39:41 +0300

    remove unused dependencies and hide .env

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-	implementation("org.springframework.security:spring-security-oauth2-jose")
 	implementation("org.springframework.security:spring-security-oauth2-resource-server")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
